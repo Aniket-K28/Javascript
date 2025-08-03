e4b5b8764ac8aece1ae2fa670c491ffd130377a8
Aniket Mishra <93672703+Aniket-K28@users.noreply.github.com>
2025-08-03 22:03:36 +0530

    Add files via upload

---
+++ 03.conditinals.js
@@ -0,0 +1,28 @@
+// let age = 20 ;
+// if(age>=10){
+//     console.log('you are an adult');
+// }else if(age>=13){
+//     console.log("you are a teenager");
+// }else{
+//     console.log("you are a child");
+// }
+
+// let day = " sunday";
+// switch(day){
+//     case "monday":
+//     console.log("start of the week");
+//     break;
+//     case " sunday":
+//     console.log("weekend chill");
+//     default:
+//     console.log("just another day");
+// }
+
+// let marks = 39 ;
+// if(marks>=40){
+//     console.log('you are pass');
+// }else if(marks>40){
+//     console.log('you are pass');
+// }else{
+//     console.log('you fail');
+// }
